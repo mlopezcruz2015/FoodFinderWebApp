72b5f8c6695b0d87e4d2a601caa23c0fba5e3a91
Michael Lopez <michael_lopez1997@hotmail.com>
2022-05-19 20:28:24 -0400

    Worked on data validation.

--- Models/SavedFoodLocation.cs
+++ Models/SavedFoodLocation.cs
@@ -6,8 +6,16 @@ namespace FoodFinderWebApp.Models
     public class SavedFoodLocation
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Name { get; set; }
+
         public string? Address { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Category { get; set; }
 
         [Display(Name = "Date Added")]
