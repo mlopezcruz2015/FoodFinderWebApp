10ac4393186f78aaec5073286c55364fd5260fd0
Michael Lopez <michael_lopez1997@hotmail.com>
2022-05-25 19:32:58 -0400

    Added api reuest to get lat long based on zip.

---
+++ Models/JSONModels.cs
@@ -0,0 +1,25 @@
+﻿namespace FoodFinderWebApp.Models
+{
+    public class JSONModels
+    {
+    }
+
+    public class ZIPLocation
+    {
+        public string country { get; set; }
+        public string countryCode2 { get; set; }
+        public string countryCode3 { get; set; }
+        public string state { get; set; }
+        public string stateCode2 { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string zipCode { get; set; }
+        public string city { get; set; }
+    }
+
+    public class ZIPRoot
+    {
+        public bool success { get; set; }
+        public List<ZIPLocation> location { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using FoodFinderWebApp.Data;
 using FoodFinderWebApp.Models;
 using Google.Maps;
+using System.Net;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FoodFinderWebAppContext>(options =>
@@ -39,14 +41,24 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//Get latlong from zip:
+
+HttpWebRequest zipRequest = (HttpWebRequest)WebRequest.Create("https://thezipcodes.com/api/v1/search?zipCode=33463&countryCode=US&apiKey=1e77aaee2bd54aafa23b46c9f14cc543");
+var zipResponse = zipRequest.GetResponse();
+var tempStr = zipResponse.GetResponseStream();
+StreamReader reader = new StreamReader(tempStr);
+var anotherTemp = reader.ReadToEnd();
+var zipInfo = JsonConvert.DeserializeObject<ZIPRoot>(anotherTemp);
+
 //Set up Google API
 GoogleSigned.AssignAllServices(new GoogleSigned("AIzaSyAnl6DeaeqQAMLAOthZjCHlaBeppbig998"));
 
 
 Google.Maps.Places.PlacesService temp = new Google.Maps.Places.PlacesService();
 var request = new Google.Maps.Places.NearbySearchRequest();
-request.Location = new LatLng(26.593694196025627, -80.14389127523728);
-request.Radius = 500;
+
+request.Location = new LatLng(Convert.ToDecimal(zipInfo.location[0].latitude), Convert.ToDecimal(zipInfo.location[0].longitude));
+request.Radius = 5000;
 request.Keyword = "Food";
 var temp2 = temp.GetResponse(request);
 
